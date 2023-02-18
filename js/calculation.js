e2aa53bf147a5a671e75b4d62a1b4878bda28877
md muhaiminul <muhai4959@gmail.com>
2023-02-18 08:10:45 +0600

    add parallelogram, rhombus, pentagon, ellipse area calculation

--- js/calculation.js
+++ js/calculation.js
@@ -20,3 +20,48 @@ document.getElementById("calculate-rectangle").addEventListener("click", functio
     //return result to area calculation field
     returnAreaResultElement(rectangle, area)
 })
+//parallelogram
+document.getElementById("parallelogram-calculate").addEventListener("click", function(){
+    const parallelogram = getGeometryName("parallelogram");
+    //get area value
+    const b = getElementValueById("parallel-base");
+    const h = getElementValueById("parallel-height");
+    //calculate area
+    const area = b * h;
+    //return result to area collection field
+    returnAreaResultElement(parallelogram, area);
+})
+//rhombus
+document.getElementById("rhombus-calculate").addEventListener("click", function(){
+    const rhombus = getGeometryName("rhombus");
+    //get area value
+    const d1 = getElementValueById("rhombus-d1");
+    const d2 = getElementValueById("rhombus-d2");
+    //calculate area
+    const area = 0.5 * d1 * d2;
+    //return result to area collection field
+    returnAreaResultElement(rhombus, area);
+})
+//pentagon
+document.getElementById("pentagon-calculate").addEventListener("click", function(){
+    const pentagon = getGeometryName("pentagon");
+    //get area value
+    const p = getElementValueById("pentagon-p");
+    const b = getElementValueById("pentagon-b");
+    //calculate area
+    const area = 0.5 * p * b;
+    //return result to area collection field
+    returnAreaResultElement(pentagon, area);
+})
+//parallelogram
+document.getElementById("ellipse-calculate").addEventListener("click", function(){
+    const ellipse = getGeometryName("ellipse");
+    //get area value
+    const a = getElementValueById("ellipse-a");
+    const b = getElementValueById("ellipse-b");
+    //calculate area
+    const area = Math.PI * a * b;
+    const areaFinal = parseFloat(area.toFixed(2));
+    //return result to area collection field
+    returnAreaResultElement(ellipse, areaFinal);
+})
